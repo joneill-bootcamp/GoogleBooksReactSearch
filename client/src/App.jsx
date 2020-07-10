23a6028e9471c3b8cd5eb4aa9a44131fa9123600
John O'Neil <fluffywestie@tpg.com.au>
2020-07-10 22:59:18 +1000

    Sort of runs, doesn't save though

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,27 +10,6 @@ import Search from "./pages/Search";
 // import NoMatch from "./pages/NoMatch";
 // import Nav from "./components/Nav";
 
-// function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <Nav />
-//         <Switch>
-//           <Route exact path={["/", "/books"]}>
-//             <Books />
-//           </Route>
-//           <Route exact path="/books/:id">
-//             <Detail />
-//           </Route>
-//           <Route>
-//             <NoMatch />
-//           </Route>
-//         </Switch>
-//       </div>
-//     </Router>
-//   );
-// }
-
 function App() {
   return (
     <Router>
