d169e3c926c0e997df6c43cb462895fe75886b5b
John O'Neil <fluffywestie@tpg.com.au>
2020-07-10 21:54:16 +1000

    Updates to Model

---
+++ models/favouriteBook.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  //synopsis: String,
+  description: {
+    type: String,
+    required: true
+  },
+  thumbnail: {
+    type: String,
+    required: true
+  },
+  href: {
+    type: String,
+    required: true
+  },
+});
+
+const Book = mongoose.model("Favourite", bookSchema);
+
+module.exports = Book;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//boilerplate
 const express = require("express");
 
 const mongoose = require("mongoose");
@@ -6,7 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -18,7 +21,13 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
